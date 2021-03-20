fa1d22e294922384111880dbc69f39664073a757
Dmitrii Stepanov <trash@mail.ru>
2021-03-20 23:23:36 +0300

    fix posix queue

--- mqueue/posix.cpp
+++ mqueue/posix.cpp
@@ -7,7 +7,12 @@
 #include <iostream>
 
 int main(){
-    mqd_t qid = mq_open("/test.mq", O_RDONLY);
+    mq_attr attr;
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 8192;
+    attr.mq_curmsgs = 0;
+    mqd_t qid = mq_open("/test.mq", O_CREAT | O_RDONLY, 0777, &attr);
     if(qid == -1){
         std::cout << "error mq_open: " << errno << std::endl;
     }
