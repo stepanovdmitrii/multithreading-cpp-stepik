fe0955e471e5549551df1ea6e8a49d8b4d6a770a
Dmitrii Stepanov <trash@mail.ru>
2021-03-20 19:34:34 +0300

    fifo

--- channels/fifo.cpp
+++ channels/fifo.cpp
@@ -4,25 +4,32 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define IN_FIFO "/home/box/in.fifo"
+#define OUT_FIFO "/home/box/out.fifo"
+
 int main(){
-    if(-1 == mkfifo("/home/box/in.fifo", O_RDWR)){
+    unlink(IN_FIFO);
+    unlink(OUT_FIFO);
+
+    if(-1 == mkfifo(IN_FIFO, 0666)){
         return -1;
     }
-    if(-1 == mkfifo("/home/box/out.fifo", O_RDWR)){
+    if(-1 == mkfifo(OUT_FIFO, 0666)){
         return -1;
     }
 
-    int in = open("/home/box/in.fifo", O_RDONLY | O_NONBLOCK);
-    int out = -1;
+    int in = open(IN_FIFO, O_RDONLY);
+    int out = open(OUT_FIFO, O_WRONLY);
 
     char buffer[512];
     int bytes_read = -1;
 
-    while((bytes_read = read(in, &buffer, 512)) > 0){
-        if(out == -1){
-            out = open("/home/box/out.fifo", O_WRONLY);
-        }
+    while(bytes_read = read(in, &buffer, 512)){
         write(out, buffer, bytes_read);
     }
-    return -1;
+    close(in);
+    close(out);
+    unlink(IN_FIFO);
+    unlink(OUT_FIFO);
+    return 0;
 }
